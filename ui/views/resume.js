56ae54a0eea0b5ff8527fdbf4c2d0b924ff69009
alo <0xyoyobojo@gmail.com>
2024-01-04 20:53:53 -0500

    update to content

--- ui/views/resume.js
+++ ui/views/resume.js
@@ -18,22 +18,22 @@ const Resume = () => {
                 </ul>
               </div>
               <div className="resume-item">
-                <div className="date">2011 - 2012</div>
-                <div className="name">Front-end Developer - Google Inc</div>
-                <p>
-                  Duis ipsum ligula, feugiat nec auctor vitae, porta sit amet
-                  urna. Praesent ex lorem, porta nec aliquet eu, ullamcorper
-                  eget nibh.
-                </p>
+                <div className="date">June 2021 - Aug. 2022</div>
+                <div className="name">Consultant & Software Engineer | IBM iX</div>
+                <ul className="dotted-list">
+                  <li>Managing a team of 4 developers while leading all sales and design pitches</li>
+                  <li>Built unique websites and CRUD apps with technologies including Django, Flask, VueJS, AngularJS on AWS web services</li>
+                  <li>Designed 20+ interactive wireframes and mockups using Sketch and InVision of which 10+ were used</li>
+                </ul>
               </div>
               <div className="resume-item">
                 <div className="date">2009 - 2010</div>
-                <div className="name">Senior Developer - Abc Inc</div>
-                <p>
-                  Morbi nulla arcu, pellentesque sed egestas in, tempor eget
-                  felis. Nullam tincidunt augue in leo feugiat, quis interdum
-                  nisi sollicitudin.
-                </p>
+                <div className="name">Lead Front-End Developer | Raeri</div>
+                <ul className="dotted-list">
+                  <li>Managing a team of 4 developers while leading all sales and design pitches</li>
+                  <li>Built unique websites and CRUD apps with technologies including Django, Flask, VueJS, AngularJS on AWS web services</li>
+                  <li>Designed 20+ interactive wireframes and mockups using Sketch and InVision of which 10+ were used</li>
+                </ul>
               </div>
             </div>
           </div>
@@ -43,22 +43,33 @@ const Resume = () => {
             </div>
             <div className="resume-items">
               <div className="resume-item">
-                <div className="date">2007 - 2008</div>
-                <div className="name">Art University of New York</div>
+                <div className="date">Sept. 2017 - May 2021</div>
+                <div className="name">New York University</div>
                 <p>
-                  Morbi nulla arcu, pellentesque sed egestas in, tempor eget
-                  felis. Nullam tincidunt augue in leo feugiat, quis interdum
-                  nisi sollicitudin.
+                  B.A. in Economics and a Joint-Minor in Computer Science and 
+                  Mathematics from New York University's College of Arts and Sciences
+                </p>
+              </div>
+            </div>
+            <div className="title mt-4 pt-4">
+              <div className="title_inner mb-1">Extracurricular</div>
+            </div>
+            <div className="resume-items">
+              <div className="resume-item">
+                <div className="date">March 2020 - March 2021</div>
+                <div className="name">NYU Entrepreneurship Club</div>
+                <p>
+                  B.A. in Economics and a Joint-Minor in Computer Science and 
+                  Mathematics from New York University's College of Arts and Sciences
                 </p>
               </div>
               <div className="resume-item">
                 <div className="date">2004 - 2006</div>
-                <div className="name">Front-end Course</div>
-                <p>
-                  Duis ipsum ligula, feugiat nec auctor vitae, porta sit amet
-                  urna. Praesent ex lorem, porta nec aliquet eu, ullamcorper
-                  eget nibh.
-                </p>
+                <div className="name">NYU Men’s Varsity Soccer</div>
+                <ul className="dotted-list">
+                  <li>Competed in the NCAA Division III and NCAA National Tournament, achieving UAA All-Academic</li>
+                  <li>Dedicated over 35 hours a week to practice, lifting, team meetings, traveling, and competitions nation-wide</li>
+                </ul>
               </div>
             </div>
           </div>
